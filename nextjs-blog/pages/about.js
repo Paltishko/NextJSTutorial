0cca0b619318cb6589aece47f7c6963b83b01279
Anton Tretiak <anton.tretiak@eventussystems.com>
2023-08-15 15:39:29 +0300

    redirection

---
+++ nextjs-blog/pages/about.js
@@ -0,0 +1,12 @@
+// export default function Post() {
+//     return (<h1>About Page</h1>);
+// }
+
+export async function getStaticProps(context) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: true, // triggers 308
+      },
+    };
+  }
